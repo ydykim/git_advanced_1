de3fcd645a00f7014837054aea3d0b9bcd4594fb
YongSang Shin <73946233+ydykim@users.noreply.github.com>
2023-03-30 19:00:08 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from typing import List
+from even_list import even_list
+from sum_of_squares_of_even import sum_of_squares_of_even
+
+# Main function
+def main():
+    # Example list
+    int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    even_int_list = even_list(int_list)
+    output = sum_of_squares_of_even(even_int_list)
+    print(output)
+# Boilerplate code
+if __name__ == "__main__":
+    main()
